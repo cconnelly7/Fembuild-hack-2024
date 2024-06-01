023d68afe7c96b77ed38646e126abf3e756b142a
cconnelly7 <chiannerose@gmail.com>
2024-06-01 14:34:17 -0400

    react webcam

---
+++ src/app/Camera.tsx
@@ -0,0 +1,31 @@
+// @ts-nocheck
+import {useRef} from 'react'
+import Webcam from "react-webcam";
+
+const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: "user"
+  };
+
+export default function Camera(){
+  return (
+    <Webcam
+    audio={false}
+    height={720}
+    screenshotFormat="image/jpeg"
+    width={1280}
+    videoConstraints={videoConstraints}
+  >
+    {({ getScreenshot }) => (
+      <button
+        onClick={() => {
+          const imageSrc = getScreenshot()
+        }}
+      >
+        Capture photo
+      </button>
+    )}
+  </Webcam>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Camera from './Camera'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        
+        <Camera/>
       </div>
     </main>
   );
