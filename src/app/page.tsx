4873d85dc86f24c598e1f6836a3ec4833877f700
cconnelly7 <chiannerose@gmail.com>
2024-06-01 18:01:57 -0400

    garment display and submit btn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,10 @@ import React, { useState } from "react";
 export default function Home() {
   const [img, setImg] = useState<string>('');
 
+  const images = require.context('../../public/garments', true);
+  const imageList = images.keys().map(image => images(image));
+  console.log('hii ', imageList);
+
   function encodeImageFileAsURL(element: any) {
     setImg(URL.createObjectURL(element.target.files[0]));
     var file = element.target.files[0];
@@ -18,12 +22,19 @@ export default function Home() {
   }
 
   return (
-    <main className={styles.main}>
-      <div>Click the upload icon below to upload a file.</div>
-      <div>
-        <input id="file-input" type="file" onChange={encodeImageFileAsURL}/>
+    <div className={styles.main}>
+      <div className={styles.selfieWrapper}>
+        <div>Upload an image of yourself including your torso.</div>
+          <input id="file-input" type="file" onChange={encodeImageFileAsURL}/>
+        <img src={img} />
       </div>
-      <img src={img} />
-    </main>
+      <div className={styles.selfieWrapper}>
+        <div>Pick a garment.</div>
+        {imageList.map((image, index) => (
+        <img key={index} src={image.default.src} width={250} height={250} alt={`image-${index}`} />
+      ))}
+      </div>
+    <button className={styles.submit}>Submit</button>
+    </div>
   );
 }
