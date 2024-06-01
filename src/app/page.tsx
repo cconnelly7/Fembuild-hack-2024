189116765a4a12913d38c7508a53197a3898e342
cconnelly7 <chiannerose@gmail.com>
2024-06-01 15:00:23 -0400

    upload img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Camera from './Camera'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <Camera/>
+      <div>Click the upload icon below to upload a file.</div>
+
+      <div>
+          <input id="file-input" type="file"/>
       </div>
     </main>
   );
