07c639d114819b8e9d93830df47ff283f481fef9
cconnelly7 <chiannerose@gmail.com>
2024-06-01 19:04:43 -0400

    highlight selected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,19 +9,25 @@ export default function Home() {
   const [img, setImg] = useState<string>('');
   const [img64, setImg64] = useState('');
 
+  const [garment, setGarment] = useState('');
+  const [active, setAvtive] = useState(-1);
+
   const images = require.context('../../public/garments', true);
   const imageList = images.keys().map(image => images(image));
-  console.log('hii ', imageList);
 
   function encodeImageFileAsURL(element: any) {
+    console.log('hihi ', element.target.files[0]);
     setImg(URL.createObjectURL(element.target.files[0]));
     var file = element.target.files[0];
     var reader = new FileReader();
     reader.onloadend = function() {
       setImg64(reader.result as string);
-      console.log('RESULT', reader.result)
     }
     reader.readAsDataURL(file);
+  }
+
+  function encodeGarment(e: any, index: number) {
+    setAvtive(index);
   }
 
   return (
@@ -34,7 +40,7 @@ export default function Home() {
       <div className={styles.selfieWrapper}>
         <div>Pick a garment.</div>
         {imageList.map((image, index) => (
-        <img key={index} src={image.default.src} width={250} height={250} alt={`image-${index}`} />
+        <img className={active == index ? styles.highlight : styles.noborder} key={index} src={image.default.src} width={250} height={250} alt={`image-${index}`} onClick={(e) => encodeGarment(e, index)}/>
       ))}
       </div>
     <button className={styles.submit}>Submit</button>
