2b03b5cbac4798751cfd35ea7890ea5d8209ee6f
cconnelly7 <chiannerose@gmail.com>
2024-06-01 16:50:07 -0400

    img save as base 64 obj

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
+// eslint-disable-file
+'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
+import React, { useState } from "react";
 
 export default function Home() {
+  const [img, setImg] = useState<string>('');
+
+  function handleChange(e: any) {
+    setImg(URL.createObjectURL(e.target.files[0]));
+    
+}
+console.log('hereee ',img);
   return (
     <main className={styles.main}>
       <div>Click the upload icon below to upload a file.</div>
-
       <div>
-          <input id="file-input" type="file"/>
+          <input id="file-input" type="file" onChange={handleChange}/>
       </div>
     </main>
   );
