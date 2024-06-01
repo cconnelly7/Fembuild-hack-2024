ca3be94741720521780b9a95fa43a149f771175a
cconnelly7 <chiannerose@gmail.com>
2024-06-01 16:51:41 -0400

    save img as base64

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,21 @@ import styles from "./page.module.css";
 import React, { useState } from "react";
 
 export default function Home() {
-  const [img, setImg] = useState<string>('');
 
-  function handleChange(e: any) {
-    setImg(URL.createObjectURL(e.target.files[0]));
-    
-}
-console.log('hereee ',img);
+  function encodeImageFileAsURL(element: any) {
+    var file = element.target.files[0];
+    var reader = new FileReader();
+    reader.onloadend = function() {
+      console.log('RESULT', reader.result)
+    }
+    reader.readAsDataURL(file);
+  }
+
   return (
     <main className={styles.main}>
       <div>Click the upload icon below to upload a file.</div>
       <div>
-          <input id="file-input" type="file" onChange={handleChange}/>
+          <input id="file-input" type="file" onChange={encodeImageFileAsURL}/>
       </div>
     </main>
   );
